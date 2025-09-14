57911a08c7bd21484bf80c085a91b2738aa6f322
pauguz <paulo.guzman1@unmsm.edu.pe>
2025-09-14 17:57:17 -0500

    modificar AgenteC

--- MultiAgente/src/agentes/AgenteC.java
+++ MultiAgente/src/agentes/AgenteC.java
@@ -1,8 +1,6 @@
 package agentes;
 
 import jade.core.Agent;
-import jade.core.behaviours.CyclicBehaviour;
-import jade.core.behaviours.WakerBehaviour;
 import jade.lang.acl.ACLMessage;
 
 public class AgenteC extends Agent {
@@ -10,34 +8,24 @@ public class AgenteC extends Agent {
     protected void setup() {
         System.out.println(getLocalName() + " (Repartidor) iniciado.");
 
-        addBehaviour(new CyclicBehaviour(this) {
-            @Override
-            public void action() {
-                ACLMessage entrega = blockingReceive();
-                if (entrega != null && entrega.getPerformative() == ACLMessage.REQUEST) {
-                    System.out.println(getLocalName() + ": recibido -> " + entrega.getContent());
+        while (true) {
+            ACLMessage mensaje = blockingReceive();
 
-                    // Necesitamos una copia final para usar dentro del WakerBehaviour
-                    final ACLMessage msg = entrega;
+            if (mensaje != null && mensaje.getPerformative() == ACLMessage.REQUEST) {
+                System.out.println(getLocalName() + ": recibido -> " + mensaje.getContent());
 
-                    // Simula tiempo de entrega sin bloquear el thread principal del agente
-                    addBehaviour(new WakerBehaviour(myAgent, 3000) {
-                        @Override
-                        protected void onWake() {
-                            System.out.println(getAgent().getLocalName() + ": entrega completada al cliente.");
+                try {
+                    // Simular tiempo de entrega
+                    System.out.println(getLocalName() + ": en camino con el pedido...");
+                    Thread.sleep(3000); // 3 segundos
+                    System.out.println(getLocalName() + ": entrega completada al cliente.");
 
-                            ACLMessage confirm = msg.createReply();
-                            confirm.setPerformative(ACLMessage.INFORM);
-                            confirm.setContent("Entrega completada para: " + msg.getContent());
-                            myAgent.send(confirm);
-                        }
-                    });
-                } else {
-                    block();
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
-        });
+        }
     }
 }
-
 
